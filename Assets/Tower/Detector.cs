0f2b26f14af399f0a757603631b15a9dbf43a49e
김희준 <wnsl3462@nate.com>
2026-06-18 15:50:28 +0900

    [김희준][GamePlay] Detector 로직 구현

---
+++ Assets/Tower/Detector.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Detector
+{
+    private Transform _ownerTransform;
+    private float _detectionRange;
+    private LayerMask _enemyLayer;
+
+    public Detector(Transform ownerTransform, float detectionRange, LayerMask enemyLayer)
+    {
+        _ownerTransform = ownerTransform;
+        _detectionRange = detectionRange;
+        _enemyLayer = enemyLayer;
+    }
+
+    public Collider[] FindEnemiesInRange()
+    {
+        Collider[] enemyColliders = Physics.OverlapSphere(_ownerTransform.position, _detectionRange, _enemyLayer);
+        return enemyColliders;
+    }
+
+    public Transform FindClosestEnemy()
+    {
+        Collider[] enemyColliders = FindEnemiesInRange();
+
+        Transform closestEnemy = null;
+        float closestDistance = float.MaxValue;
+
+        for(int i = 0; i < enemyColliders.Length; i++)
+        {
+            Transform enemyTransform = enemyColliders[i].transform;
+            float distanceToEnemy = Vector3.Distance(_ownerTransform.position, enemyTransform.position);
+
+            if(distanceToEnemy < closestDistance)
+            {
+                closestDistance = distanceToEnemy;
+                closestEnemy = enemyTransform;
+            }
+        }
+
+        return closestEnemy;
+    }
+
+}
