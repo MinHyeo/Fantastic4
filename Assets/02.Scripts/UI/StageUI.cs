f209966f7629b3095284fb58271bdd5565d13650
이선학 <mumahub1219@gmail.com>
2026-06-18 18:10:21 +0900

    [이선학][UI] 스테이지 UI 코드
    깨지 않은 스테이지 암흑 처리, 버튼 비활성화

--- Assets/02.Scripts/UI/StageUI.cs
+++ Assets/02.Scripts/UI/StageUI.cs
@@ -1,18 +1,68 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class StageUI : MonoBehaviour
+public class StageUI : UIBase
 {
-    [SerializeField] private DaniTechUIButton Button_Stage1;
-    [SerializeField] private DaniTechUIButton Button_Stage2;
-    [SerializeField] private DaniTechUIButton Button_Stage3;
-    [SerializeField] private DaniTechUIButton Button_Stage4;
-    [SerializeField] private DaniTechUIButton Button_Stage5;
-
+    [SerializeField] private DaniTechUIButton[] Button_Stage;
     [SerializeField] private DaniTechUIButton Button_Back;
+
+    [SerializeField] private Color LockColor = new Color(0.3f, 0.3f, 0.3f, 1f);
+    [SerializeField] private Color UnLockColor = Color.white;
 
     private void OnEnable()
     {
         Button_Back.BindOnClickButtonEvent(OnClickReturnLobby);
+
+        RefreshStageButton();
+    }
+
+    private void RefreshStageButton()
+    {
+        int clearStage = PlayerPrefs.GetInt("ClearStage", 0);
+
+        int openStageIndex = clearStage;
+
+        for (int i = 0; i < Button_Stage.Length; i++)
+        {
+            Button buttonComponent = Button_Stage[i].GetComponent<Button>();
+            Image imageComponent = Button_Stage[i].GetComponent<Image>();
+
+            if (i <= openStageIndex)
+            {
+                if (buttonComponent != null)
+                {
+                    buttonComponent.interactable = true;
+                }
+                if (imageComponent != null)
+                {
+                    imageComponent.color = UnLockColor;
+                }
+
+                StageButton info = Button_Stage[i].GetComponent<StageButton>();
+                if (info == null)
+                {
+                    info = Button_Stage[i].gameObject.AddComponent<StageButton>();
+                }
+
+                info.InitStageButton(i + 1, this);
+            }
+            else
+            {
+                if (buttonComponent != null)
+                {
+                    buttonComponent.interactable = false;
+                }
+                if (imageComponent != null)
+                {
+                    imageComponent.color = LockColor;
+                }
+            }
+        }
+    }
+
+    public void OnClickStageOpen(int stageNumber)
+    {
+        // Todo : 스테이지 시작 로직 넣기
     }
 
     private void OnClickReturnLobby()
