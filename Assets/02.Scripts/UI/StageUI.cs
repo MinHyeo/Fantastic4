2f8c3a1210198342cc6edef6100a30664b02e689
이선학 <mumahub1219@gmail.com>
2026-06-18 17:12:28 +0900

    [이선학][UI] 스테이지 UI 기본 코드 작성

---
+++ Assets/02.Scripts/UI/StageUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class StageUI : MonoBehaviour
+{
+    [SerializeField] private DaniTechUIButton Button_Stage1;
+    [SerializeField] private DaniTechUIButton Button_Stage2;
+    [SerializeField] private DaniTechUIButton Button_Stage3;
+    [SerializeField] private DaniTechUIButton Button_Stage4;
+    [SerializeField] private DaniTechUIButton Button_Stage5;
+
+    [SerializeField] private DaniTechUIButton Button_Back;
+
+    private void OnEnable()
+    {
+        Button_Back.BindOnClickButtonEvent(OnClickReturnLobby);
+    }
+
+    private void OnClickReturnLobby()
+    {
+        // UIManager.Instance.OpenUI(UIRootType.MainUI, UIType.LobbyUI);
+        // UIManager.Instance.CloseContentUI(UIType.StageUI);
+    }
+}
