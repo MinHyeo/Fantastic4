48c81241e08ebe812dcef2281d26bc775d92dd0e
이선학 <mumahub1219@gmail.com>
2026-06-18 16:36:25 +0900

    [이선학][UI] 메인 UI, 타워덱 기본 코드 작성

---
+++ Assets/02.Scripts/UI/MainUI_Tower.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class MainUI_Tower : MonoBehaviour
+{
+    [SerializeField] private DaniTechUIButton Button_Pause;
+    [SerializeField] private TextMeshProUGUI Text_Wave;
+    [SerializeField] private TextMeshProUGUI Text_Gold;
+    [SerializeField] private GameObject TowerDeck;
+
+    private void OnEnable()
+    {
+        Button_Pause.BindOnClickButtonEvent(OnClickPauseGame);
+    }
+    
+    private void OnClickPauseGame()
+    {
+        // UIManager.Instance.OpenPopupUI(UIType.PauseUI);
+        // Todo : 게임 일시정지 기능도 넣어야 함.
+    }
+
+    private void InitMainUI()
+    {
+
+    }
+
+}
