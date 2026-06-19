a562b590045dba6f76465994c097311789a15bf4
김희준 <wnsl3462@nate.com>
2026-06-19 18:15:19 +0900

    [김희준][GamePlay] AttackSystem 코드 작성, Projectile 스크립트 생성

---
+++ Assets/Tower/AttackSystem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public static class AttackSystem
+{
+   public static void Attack(GameObject projectile, Transform towerTransform, Transform targetTransform, int damage)
+    {
+        GameObject spawnedProjectile = Object.Instantiate(projectile, towerTransform.position, towerTransform.rotation);
+
+        Projectile projectileScript = spawnedProjectile.GetComponent<Projectile>();
+        projectileScript.Initialize(damage, targetTransform);
+    }
+}
