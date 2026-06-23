22d1e58d63a04ee9efff54df5b52b012d67e8429
최진영 <jychoi194@gmail.com>
2026-06-23 16:59:49 +0900

    [최진영][GamePlay] enemy_01 ID 하드코딩으로 이동속도 데이터 연동 구현
    웨이브/스폰 시스템이 아직 없어 enemyId는 "enemy_01"로 임시 하드코딩

--- Assets/02.Scripts/Enemy/NormalEnemy.cs
+++ Assets/02.Scripts/Enemy/NormalEnemy.cs
@@ -2,10 +2,16 @@
 
 public class NormalEnemy : EnemyBase
 {
-    [SerializeField] private float _moveSpeed = 5f;
-    [SerializeField] private Transform _tempPosition;
+    [SerializeField] private string _enemyId = "Enemy_01"; // 임시 하드코딩, 추후 웨이브에서 전달받을 값으로 교체
+    [SerializeField] private Transform _tempPosition; //권동님 웨이포인트 매니저에서 호출돼야 함
 
-    //권동님 웨이포인트 매니저에서 호출돼야 함
+    private float _moveSpeed;
+
+    private void Start()
+    {
+        EnemyData enemyData = GameDataManager.Instance.GetData<EnemyData>(_enemyId);
+        _moveSpeed = enemyData.MoveSpeed;
+    }
 
     private void FixedUpdate()
     {
