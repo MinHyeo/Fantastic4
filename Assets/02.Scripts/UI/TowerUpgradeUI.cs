fd5bedefcf55faa90f226f26ee37acdbb4f09c11
이선학 <mumahub1219@gmail.com>
2026-06-24 16:32:36 +0900

    [이선학][UI] 타워 강화창 UI 기본 스크립트 작성

---
+++ Assets/02.Scripts/UI/TowerUpgradeUI.cs
@@ -0,0 +1,42 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TowerUpgradeUI : UIBase
+{
+    [Header("버튼")]
+    [SerializeField] private UIButton Button_CloseBG;
+    [SerializeField] private UIButton Button_Close;
+    [SerializeField] private UIButton Button_Upgrade;
+
+    [Header("텍스트")]
+    [SerializeField] private TextMeshProUGUI Text_LevelBefore;
+    [SerializeField] private TextMeshProUGUI Text_DamageBefore;
+    [SerializeField] private TextMeshProUGUI Text_AttackSpeedBefore;
+
+    [SerializeField] private TextMeshProUGUI Text_LevelAfter;
+    [SerializeField] private TextMeshProUGUI Text_DamageAfter;
+    [SerializeField] private TextMeshProUGUI Text_AttackSpeedAfter;
+
+    private void OnEnable()
+    {
+        Button_CloseBG.BindOnClickButtonEvent(OnClickCloseTowerUpgradeUI);
+        Button_Close.BindOnClickButtonEvent(OnClickCloseTowerUpgradeUI);
+        Button_Upgrade.BindOnClickButtonEvent(OnClickTowerUpgrade);
+    }
+
+    private void OnClickCloseTowerUpgradeUI()
+    {
+        UIManager.Instance.ClosePopupUI(UIType.TowerUpgradeUI);
+    }
+
+    private void OnClickTowerUpgrade()
+    {
+        UpdateTowerUpgradeInfo();
+
+    }
+
+    private void UpdateTowerUpgradeInfo()
+    {
+
+    }
+}
