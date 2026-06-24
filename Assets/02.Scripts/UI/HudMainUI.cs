0540b9575d13d26915e352b4f792e23b3c8f88d4
이선학 <mumahub1219@gmail.com>
2026-06-24 17:09:44 +0900

    [이선학][UI] HUD 스크립트 작성

---
+++ Assets/02.Scripts/UI/HudMainUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HudMainUI : UIBase
+{
+    [SerializeField] private GameObject Prefab_HudSlot;
+    [SerializeField] private Transform Transform_SlotRoot;
+
+    private Dictionary<int, HudSlotUI> _hudSlotList = new Dictionary<int, HudSlotUI> ();
+
+    public void AddHudSlot(int instanceId, Transform targetTransform)
+    {
+        CreateHudSlot(instanceId, targetTransform);
+    }
+
+    private void CreateHudSlot(int instanceId, Transform targetTransform)
+    {
+        var gObj = Instantiate(Prefab_HudSlot, Transform_SlotRoot);
+        if (gObj == null) return;
+
+        var slotComponent = gObj.GetComponent<HudSlotUI>();
+        if (slotComponent == null) return;
+
+        slotComponent.InitSlot(instanceId, targetTransform);
+
+        _hudSlotList.Add(instanceId, slotComponent);
+    }
+
+    public void RemoveHudSlot(int instanceId)
+    {
+        if (_hudSlotList.ContainsKey(instanceId) == true)
+        {
+            var slot = _hudSlotList[instanceId];
+
+            Destroy(slot.gameObject);
+            _hudSlotList.Remove(instanceId);
+        }
+    }
+}
