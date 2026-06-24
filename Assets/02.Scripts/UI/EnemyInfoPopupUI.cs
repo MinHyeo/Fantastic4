44fc54694aa78eaa31b0050b9ac5c8de3102ff3e
SonghaCH <cwithus0002@gmail.com>
2026-06-24 17:48:02 +0900

    [차송하][UI] 정보창 구조 수정
    데이터 드리븐 이후 수정 예정 (목요일)

--- Assets/02.Scripts/UI/EnemyInfoPopupUI.cs
+++ Assets/02.Scripts/UI/EnemyInfoPopupUI.cs
@@ -1,6 +1,31 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyInfoPopupUI : UIBase
 {
-    
+    [SerializeField] private UIButton Button_TowerInfo;
+    [SerializeField] private UIButton Button_Enemy1;
+    [SerializeField] private UIButton Button_Enemy2;
+    [SerializeField] private UIButton Button_Enemy3;
+    [SerializeField] private UIButton Button_Enemy4;
+    [SerializeField] private UIButton Button_Enemy5;
+
+    [SerializeField] private TextMeshProUGUI Text_EnemyName1;
+    [SerializeField] private TextMeshProUGUI Text_EnemyName2;
+    [SerializeField] private TextMeshProUGUI Text_EnemyName3;
+    [SerializeField] private TextMeshProUGUI Text_EnemyName4;
+    [SerializeField] private TextMeshProUGUI Text_EnemyName5;
+
+    [SerializeField] private Image Image_Enemy1;
+    [SerializeField] private Image Image_Enemy2;
+    [SerializeField] private Image Image_Enemy3;
+    [SerializeField] private Image Image_Enemy4;
+    [SerializeField] private Image Image_Enemy5;
+
+
+
+
+
+
 }
