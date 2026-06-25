9b7992b2d781af70c6315e2c81df6b83c873d96e
유민형 <minhyeong61022@gmail.com>
2026-06-26 02:12:28 +0900

    [유민형][GamePlay] 포탑 및 스테이지 오브젝트 생성 구현

--- Assets/02.Scripts/Common/GameObjectManager.cs
+++ Assets/02.Scripts/Common/GameObjectManager.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 
 public class GameObjectManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _prefabEnemy;
-    [SerializeField] private Transform _rootEnemy;
+    [SerializeField] private Transform _root;
 
     public static GameObjectManager Instance { get; set; }
 
@@ -39,6 +38,7 @@ public class GameObjectManager : MonoBehaviour
         return spawnObject;
     }
 
+    // 적 소환
     public async UniTaskVoid CreateEnemyObject(string enemyId, Vector3 spawnSpot)
     {
         // 데이터 유무 확인
@@ -51,7 +51,7 @@ public class GameObjectManager : MonoBehaviour
         GameObject enemyObject = GetGameObjectInObjectPool(enemyId);
         if(enemyObject == null)
         {
-            enemyObject = await ResourceManager.Instance.InstantiateAsync(enemyData.PrefabPath, _rootEnemy, true);
+            enemyObject = await ResourceManager.Instance.InstantiateAsync(enemyData.PrefabPath, _root, true);
         }
         enemyObject.SetActive(true);
         enemyObject.transform.position = spawnSpot;
@@ -69,5 +69,67 @@ public class GameObjectManager : MonoBehaviour
             enemyObject.Init(enemyId);
             _activeObjectList.Add(createdObject);
         }
+    }
+
+    // 타워 소환
+    public async UniTaskVoid CreateTowerObject(string towerId, Vector3 spawnSpot)
+    {
+        // 데이터 유무 확인
+        var towerData = GameDataManager.Instance.GetData<TowerData>(towerId);
+        if (towerData == null)
+            return;
+
+        // ObjectPool에 존재하는지 확인
+        // 없으면 Queue 초기화
+        GameObject towerObject = GetGameObjectInObjectPool(towerId);
+        if (towerObject == null)
+        {
+            towerObject = await ResourceManager.Instance.InstantiateAsync(towerData.PrefabPath, _root, true);
+        }
+        towerObject.SetActive(true);
+        towerObject.transform.position = spawnSpot;
+        AddTowerObjectOnCreate(towerObject, towerId);
+    }
+
+    private void AddTowerObjectOnCreate(GameObject createdObject, string towerId)
+    {
+        _objectInstanceKeyGenerator++;
+        var generatedInstanceId = _objectInstanceKeyGenerator;
+        var towerObject = createdObject.GetComponent<NormalEnemy>();
+
+        if (towerObject != null)
+        {
+            towerObject.Init(towerId);
+            _activeObjectList.Add(createdObject);
+        }
+    }
+
+    // 스테이지 소환
+    public async UniTaskVoid CreateStageObject(string stageId, Vector3 spawnSpot)
+    {
+        // 데이터 유무 확인
+        var stageData = GameDataManager.Instance.GetData<StageData>(stageId);
+        if (stageData == null)
+            return;
+
+        // ObjectPool에 존재하는지 확인
+        // 없으면 Queue 초기화
+        GameObject stageObject = GetGameObjectInObjectPool(stageId);
+        if (stageObject == null)
+        {
+            stageObject = await ResourceManager.Instance.InstantiateAsync(stageData.PrefabPath, _root, true);
+        }
+        stageObject.SetActive(true);
+        stageObject.transform.position = spawnSpot;
+        AddStageObjectOnCreate(stageObject, stageId);
+    }
+
+    private void AddStageObjectOnCreate(GameObject createdObject, string stageId)
+    {
+        _objectInstanceKeyGenerator++;
+        var generatedInstanceId = _objectInstanceKeyGenerator;
+
+        StageManager.Instance.StartStage(stageId, createdObject);
+        _activeObjectList.Add(createdObject);
     }
 }
