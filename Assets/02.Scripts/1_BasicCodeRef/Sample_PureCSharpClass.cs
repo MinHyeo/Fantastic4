0ecfe22a6621bd63373cf98a89b9c981fd7a1aeb
유민형 <minhyeong61022@gmail.com>
2026-06-25 18:27:58 +0900

    [유민형][Refactor] 필요없는 Script 파일 제거

--- Assets/02.Scripts/1_BasicCodeRef/Sample_PureCSharpClass.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class SampleBasicCode_NonMono
-{
-    public void PrintSomeText()
-    {
-        Debug.Log("출력!");
-    }
-}
--- Assets/02.Scripts/Common/UIManagerExtension.cs
+++ Assets/02.Scripts/Common/UIManagerExtension.cs
@@ -46,21 +46,6 @@ public static class UIManagerExtension
         // MainUI도
     }
 
-    public static void OpenSimplePopup(this UIManager uiManager, string msg)
-    {
-        var uiBase = uiManager.OpenPopupUI(UIType.SimplePopup);
-        if (uiBase == null)
-        {
-            Debug.LogWarning($"UI가 생성되지 않았습니다");
-            return;
-        }
-
-        if (uiBase is SimplePopup simplePopup)
-        {
-            simplePopup.SetUI(msg);
-        }
-    }
-
     // 신규UI추가 3) 이렇게 어떤 팝업을 열고, 열때 전달해야하는 파라미터가 있다면 이렇게 전달한다.
     // 추가하기 편하게 그냥 빼둔 확장 메서드이므로, uiManager과 this는 우선 넘어가자
 
