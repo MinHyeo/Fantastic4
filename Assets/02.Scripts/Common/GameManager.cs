4b9b3547a2730948029d8318bdf78cca49724607
이선학 <mumahub1219@gmail.com>
2026-06-27 17:15:21 +0900

    [이선학][UI] 스테이지 UI 코드 수정

--- Assets/02.Scripts/Common/GameManager.cs
+++ Assets/02.Scripts/Common/GameManager.cs
@@ -5,8 +5,18 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; set; }
 
+    public int CurrentUnlockedStage { get; private set; } = 1;
+
     private void Awake()
     {
         Instance = this;
+    }
+
+    public void UnlockStage(int stageNumber)
+    {
+        if (stageNumber > CurrentUnlockedStage)
+        {
+            CurrentUnlockedStage = stageNumber;
+        }
     }
 }
