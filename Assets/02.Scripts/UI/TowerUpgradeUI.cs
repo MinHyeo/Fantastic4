18ff9f5c263b14bc75eb1ffa3c5a37cc96f195a8
이유찬 <ho6712073@gmail.com>
2026-06-27 21:46:15 +0900

    Revert "[이유찬][UI] 강화UI 코드작성"
    This reverts commit e54118da83b3366540b71c3adba1ed11f136dd3e.

--- Assets/02.Scripts/UI/TowerUpgradeUI.cs
+++ Assets/02.Scripts/UI/TowerUpgradeUI.cs
@@ -1,31 +1,24 @@
-﻿using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 
 public class TowerUpgradeUI : UIBase
 {
-    private enum TowerUpgradeState
-    {
-        Before = 0,
-        After = 1,
-    }
-
     [Header("버튼")]
     [SerializeField] private UIButton Button_CloseBG;
     [SerializeField] private UIButton Button_Close;
     [SerializeField] private UIButton Button_Upgrade;
 
-    [Header("텍스트 영역")]
-    [SerializeField] private List<TextMeshProUGUI> _damgeTextList = new List<TextMeshProUGUI>();
-    [SerializeField] private List<TextMeshProUGUI> _attackRangeTextList = new List<TextMeshProUGUI>();
-    [SerializeField] private List<TextMeshProUGUI> _attackSpeedTextList = new List<TextMeshProUGUI>();
-    [SerializeField] private List<TextMeshProUGUI> _projectileSpeedTextList = new List<TextMeshProUGUI>();
+    [Header("텍스트")]
+    [SerializeField] private TextMeshProUGUI Text_LevelBefore;
+    [SerializeField] private TextMeshProUGUI Text_DamageBefore;
+    [SerializeField] private TextMeshProUGUI Text_AttackSpeedBefore;
 
-
+    [SerializeField] private TextMeshProUGUI Text_LevelAfter;
+    [SerializeField] private TextMeshProUGUI Text_DamageAfter;
+    [SerializeField] private TextMeshProUGUI Text_AttackSpeedAfter;
 
     private void OnEnable()
     {
-
         Button_CloseBG.BindOnClickButtonEvent(OnClickCloseTowerUpgradeUI);
         Button_Close.BindOnClickButtonEvent(OnClickCloseTowerUpgradeUI);
         Button_Upgrade.BindOnClickButtonEvent(OnClickTowerUpgrade);
@@ -36,60 +29,14 @@ public class TowerUpgradeUI : UIBase
         UIManager.Instance.ClosePopupUI(UIType.TowerUpgradeUI);
     }
 
- 
-    public void InitUpgradeInfo(string towerId)
-    {
-        var towerData = GameDataManager.Instance.GetData<TowerData>(towerId);
-        if (towerData == null)
-        {
-            return;
-        }
-
-        ChangeText(towerData, TowerUpgradeState.Before);
-
-        string nextTowerId = towerData.UpgradeId;
-        var nextTowerData = GameDataManager.Instance.GetData<TowerData>(nextTowerId);
-        if (nextTowerData == null)
-        {
-            // 최고 레벌
-            return;
-        }
-        ChangeText(nextTowerData, TowerUpgradeState.After);
-
-    }
-
-    private void ChangeText(TowerData towerData, TowerUpgradeState tower)
-    {
-        int index = (int)tower;
-        _damgeTextList[index].text = towerData.AttackDamage.ToString();
-        _attackRangeTextList[index].text = towerData.AttackRange.ToString();
-        _attackSpeedTextList[index].text = towerData.AttackSpeed.ToString();
-        _projectileSpeedTextList[index].text = towerData.ProjectileSpeed.ToString();
-    }
-
-    private void MaxUpgradeLeve()
-    {
-        int afterIndex = (int)TowerUpgradeState.After;
-
-
-        if (_damgeTextList.Count > afterIndex) _damgeTextList[afterIndex].text = "MAX";
-        if (_attackRangeTextList.Count > afterIndex) _attackRangeTextList[afterIndex].text = "MAX";
-        if (_attackSpeedTextList.Count > afterIndex) _attackSpeedTextList[afterIndex].text = "MAX";
-        if (_projectileSpeedTextList.Count > afterIndex) _projectileSpeedTextList[afterIndex].text = "MAX";
-    
-        Button_Upgrade.gameObject.SetActive(false);
-    }
-
     private void OnClickTowerUpgrade()
     {
         UpdateTowerUpgradeInfo();
+
     }
 
     private void UpdateTowerUpgradeInfo()
     {
-        //TODO : 타워 업그레이드 로직 받아오기
 
     }
 }
-
-    
