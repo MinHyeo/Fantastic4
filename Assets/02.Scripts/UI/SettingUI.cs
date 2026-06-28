e9a87e34864f30c0a2e6da17909ec214888c205e
이선학 <mumahub1219@gmail.com>
2026-06-28 20:42:48 +0900

    [이선학][UI] 설정 UI 프리팹, 코드 작성

---
+++ Assets/02.Scripts/UI/SettingUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingUI : UIBase
+{
+    [SerializeField] private UIButton Button_Close;
+    [SerializeField] private Slider Slider_EnvironmentSound;
+    [SerializeField] private Slider Slider_EffectSound;
+
+
+    private void OnEnable()
+    {
+        Button_Close.BindOnClickButtonEvent(OnClickCloseSettingUI);
+    }
+
+    private void SoundControl()
+    {
+        // TODO : 사운드가 생기면 연동되도록 SoundManager에서 사용될 듯
+    }
+
+    private void OnClickCloseSettingUI()
+    {
+        UIManager.Instance.ClosePopupUI(UIType.SettingUI);
+    }
+}
