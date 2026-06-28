7d1776090a987b6b639d67aba326305344bce2cc
최진영 <jychoi194@gmail.com>
2026-06-28 18:58:22 +0900

    [최진영][GamePlay] 약사노움 PharmacistEnemy 클래스 구현

---
+++ Assets/02.Scripts/Enemy/PharmacistEnemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PharmacistEnemy : NormalEnemy
+{
+    private AbilityBase _ability;
+
+    public new void Init(string enemyId)
+    {
+        base.Init(enemyId);
+        _ability = new HealAbility();
+    }
+
+    protected override void Die()
+    {
+        _ability.Employ();
+        base.Die();
+    }
+}
