85de872221b75f2568c1230c387935d5e454150e
유민형 <minhyeong61022@gmail.com>
2026-07-01 17:07:13 +0900

    [유민형][Fix] 스테이지 max 레벨 설정

--- Assets/02.Scripts/Common/GameManager.cs
+++ Assets/02.Scripts/Common/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
 
     private const string SAVE_KEY = "CurrentUnlockedStage";
 
+    private int _maxStageIndex = 3;
     private int _currentUnlockedStage = 1;
     public int CurrentUnlockedStage => _currentUnlockedStage;
 
@@ -23,6 +24,9 @@ public class GameManager : MonoBehaviour
 
     public void UnlockStage(int stageNumber)
     {
+        if (_currentUnlockedStage >= _maxStageIndex)
+            return;
+
         _currentUnlockedStage = stageNumber + 1;
         SaveGameData();
     }
