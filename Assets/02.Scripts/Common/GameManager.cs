cb4f358939675dc9d31e40d67d862978f7b55d8c
SonghaCH <cwithus0002@gmail.com>
2026-07-01 16:09:16 +0900

    [차송하,이유찬][System] 스테이지 저장 로직 추가 게임매니저 코드 수정

--- Assets/02.Scripts/Common/GameManager.cs
+++ Assets/02.Scripts/Common/GameManager.cs
@@ -5,12 +5,20 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; set; }
 
+    private const string SAVE_KEY = "CurrentUnlockedStage";
+
     private int _currentUnlockedStage = 1;
     public int CurrentUnlockedStage => _currentUnlockedStage;
 
     private void Awake()
     {
         Instance = this;
+        LoadGameData(); 
+    }
+
+    private void LoadGameData()
+    {
+        _currentUnlockedStage = PlayerPrefs.GetInt(SAVE_KEY, 1);
     }
 
     public void UnlockStage(int stageNumber)
@@ -18,6 +26,14 @@ public class GameManager : MonoBehaviour
         if (stageNumber > _currentUnlockedStage)
         {
             _currentUnlockedStage = stageNumber;
+            SaveGameData();
         }
+    }
+
+    private void SaveGameData()
+    {
+        PlayerPrefs.SetInt(SAVE_KEY, _currentUnlockedStage);
+        PlayerPrefs.Save(); 
+        Debug.Log($"[GameManager] 스테이지 저장 완료: {_currentUnlockedStage}");
     }
 }
